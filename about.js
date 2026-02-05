914188d1e6488d1ff25fe9c3ea72833ec01c0701
Adrián <adrianblanco@users.noreply.github.com>
2026-02-05 14:13:41 +0100

    Update

--- about.js
+++ about.js
@@ -1,8 +1,8 @@
 // About data
 const ABOUT_DATA = {
   "paragraphs": [
-    "I am a Graphics editor at The Washington Post where I edit, analyze, code, design, illustrate and animate data-driven visual stories. My work combines journalism, analysis, design and technology to help readers understand complex topics through compelling visual narratives.",
-    "I joined The Post as a Graphics reporter in 2019 in Washington, D.C. Since December 2024, I have been based in Madrid editing and covering International and Breaking News stories from a visual and graphics perspective.",
+    "I was a Graphics editor at The Washington Post where I edited, analyzed, coded, designed, illustrated and animated data-driven visual stories. My work combined journalism, analysis, design and technology to help readers understand complex topics through compelling visual narratives.",
+    "I joined The Post as a Graphics reporter in 2019 in Washington, D.C. From December 2024, I was based in Madrid editing and covering International and Breaking News stories from a visual and graphics perspective.",
     "I hold an M.S. in Data Journalism from Columbia University where I studied with a fellowship from La Caixa Foundation. I also hold an M.B.A. in International Management from Universidad Internacional Menéndez Pelayo with a fellowship from ICEX-CECO.",
     "Before joining The Post I worked at Agencia EFE, Radio Televisión Española and El Confidencial. I also participated in the International Consortium of Investigative Journalists' Paradise Papers investigation. Back then I used to organize Journocoders Madrid.",
     "My work has been recognized by the Society of News Design (SND), the Information is Beautiful Awards, the Livingston Awards at the University of Michigan, the Overseas Press Club of America and the North American Cartographic Information Society (NACIS) among others.",
